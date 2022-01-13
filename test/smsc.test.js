f874d169ffe9f15a2d820259bfbbdd1969bc750a
Elena Tverdokhlib <elena@nitralabs.com>
2022-01-13 11:43:55 +0200

    format

--- test/smsc.test.js
+++ test/smsc.test.js
@@ -1,12 +1,12 @@
 /* global it, expect */
-import { sendSms } from '../src'
+import { sendSms } from "../src";
 
-it('success', async () => {
-  const data = await sendSms('+37120128611', 'тест')
-  expect(data.cnt).toEqual(1)
-})
+it("success", async () => {
+  const data = await sendSms("+37120128611", "тест");
+  expect(data.cnt).toEqual(1);
+});
 
-it('failure', async () => {
-  const data = await sendSms('000', 'тест')
-  expect(data.error_code).toEqual(1)
-})
+it("failure", async () => {
+  const data = await sendSms("000", "тест");
+  expect(data.error_code).toEqual(1);
+});
